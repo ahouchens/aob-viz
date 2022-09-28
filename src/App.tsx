e08fe15a87fd5eac0ec3b12f9372f08ba1401c5e
ahouchens <webtechalex@gmail.com>
2022-09-28 15:04:50 -0400

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -71,8 +71,8 @@ function App() {
           }
           //@ts-ignore
           let name = i ? i : "";
-          // let color = faker.color.rgb();
-          let color = "green";
+          let color = faker.color.rgb();
+          // let color = "green";
 
           let totalLaityCount = results.data.filter(
             (i) => i["userType"] == "Laity"
@@ -157,22 +157,22 @@ function App() {
             returnItems?.children?.push({
               name: name,
               value: count,
-              size: count,
-              loc: count,
+              // size: count,
+              // loc: count,
               color: color,
               children: [
                 {
                   name: "Laity",
-                  value: 11,
-                  size: 11,
-                  loc: percentageLaity,
+                  // value: 11,
+                  // size: 11,
+                  value: percentageLaity,
                   color: color,
                 },
                 {
                   name: "Clergy",
-                  value: 11,
-                  size: 11,
-                  loc: percentageClergy,
+                  // value: 11,
+                  // size: 11,
+                  value: percentageClergy,
                   color: color,
                 },
                 // { name: "Clergy", color: "blue", size: 11 },
@@ -236,11 +236,281 @@ function App() {
     height: 500,
     padding: 2,
     id: "name",
-    value: "loc",
+    // value: "loc",
     // labelsSkipRadius: 16,
   };
   const [zoomedId, setZoomedId] = useState<string | null>(null);
 
+  const CircleChart = (testData: Node | undefined) => {
+    return (
+      <CirclePacking
+        {...commonProperties}
+        data={testData}
+        enableLabels
+        tooltip={({ id, value, color }) => (
+          <div style={{ backgroundColor: "#666", padding: "8px" }}>
+            <strong style={{ color }}>
+              {id}: {value}
+            </strong>
+          </div>
+        )}
+        label={(input: any) => {
+          //@ts-ignore
+          return input?.data?.name?.length > 9
+            ? `${input.data?.name?.substring(0, 5)}...`
+            : input?.data?.name;
+        }}
+        labelsSkipRadius={30}
+        // colors={{ scheme: "nivo" }}
+
+        colors={(node: Node) => {
+          console.log("node", node);
+          return node.y > 5 ? "red" : "black";
+        }}
+        // colors={{ scheme: "category10" }}
+        // colors={[]}
+        // inheritColorFromParent={true}
+        colorBy="id"
+        labelsFilter={(label) => label.node.height === 0}
+        // labelComponent={(ar) => <div>{ar.label}</div>}
+        labelTextColor="black"
+        // labelTextColor={{
+        //   from: "color",
+        // }}
+        zoomedId={zoomedId}
+        motionConfig="slow"
+        onClick={(node) => {
+          setZoomedId(zoomedId === node.id ? null : node.id);
+        }}
+      />
+    );
+  };
+
+  let staticValues = {
+    children: [
+      {
+        name: "Sexual morality",
+        value: 20,
+        color: "#e45a1c",
+        children: [
+          {
+            name: "Laity",
+            value: 5.019305019305019,
+            color: "hsl(353, 70%, 50%)",
+          },
+          {
+            name: "Clergy",
+            value: 3.4246575342465753,
+            color: "hsl(353, 70%, 50%)",
+          },
+        ],
+      },
+      {
+        name: "Support for clergy (to avoid burnout)",
+        value: 72,
+        color: "hsl(353, 70%, 50%)",
+        children: [
+          {
+            name: "Laity",
+            value: 18.146718146718147,
+            color: "#550fba",
+          },
+          {
+            name: "Clergy",
+            value: 13.013698630136986,
+            color: "#550fba",
+          },
+        ],
+      },
+      {
+        name: "Strategies for parish education, catechization, outreach, etc.",
+        value: 83,
+        color: "rgb(0,0,0)",
+        children: [
+          {
+            name: "Laity",
+            value: 20.463320463320464,
+            color: "#1bc3aa",
+          },
+          {
+            name: "Clergy",
+            value: 17.123287671232877,
+            color: "#1bc3aa",
+          },
+        ],
+      },
+      {
+        name: "Financial contributions/donations/stability",
+        value: 56,
+        color: "#c21ffc",
+        children: [
+          {
+            name: "Laity",
+            value: 11.96911196911197,
+            color: "#c21ffc",
+          },
+          {
+            name: "Clergy",
+            value: 16.43835616438356,
+            color: "#c21ffc",
+          },
+        ],
+      },
+      {
+        name: "Growing the parish",
+        value: 91,
+        color: "#7163cb",
+        children: [
+          {
+            name: "Laity",
+            value: 20.84942084942085,
+            color: "#7163cb",
+          },
+          {
+            name: "Clergy",
+            value: 23.972602739726025,
+            color: "#7163cb",
+          },
+        ],
+      },
+      {
+        name: 'Depth of engagement" in parish life (frequency of confession  attendance at non-Sunday services  participation in ministries)',
+        value: 114,
+        color: "#1ecc9c",
+        children: [
+          {
+            name: "Laity",
+            value: 27.413127413127413,
+            color: "#1ecc9c",
+          },
+          {
+            name: "Clergy",
+            value: 29.45205479452055,
+            color: "#1ecc9c",
+          },
+        ],
+      },
+      {
+        name: "Mental health needs of parishioners",
+        value: 29,
+        color: "#5fbda9",
+        children: [
+          {
+            name: "Laity",
+            value: 8.108108108108109,
+            color: "#5fbda9",
+          },
+          {
+            name: "Clergy",
+            value: 4.794520547945205,
+            color: "#5fbda9",
+          },
+        ],
+      },
+      {
+        name: "Influx of new inquirers",
+        value: 44,
+        color: "#a0b16c",
+        children: [
+          {
+            name: "Laity",
+            value: 10.81081081081081,
+            color: "#a0b16c",
+          },
+          {
+            name: "Clergy",
+            value: 8.904109589041095,
+            color: "#a0b16c",
+          },
+        ],
+      },
+      {
+        name: "Youth Ministry (retention and return of youth & young adults)",
+        value: 101,
+        color: "#a8abea",
+        children: [
+          {
+            name: "Laity",
+            value: 25.482625482625483,
+            color: "#a8abea",
+          },
+          {
+            name: "Clergy",
+            value: 21.232876712328768,
+            color: "#a8abea",
+          },
+        ],
+      },
+      {
+        name: "Effects of COVID-19 (attendance, stewardship, parish life, return & retention)",
+        value: 46,
+        color: "#fad6f9",
+        children: [
+          {
+            name: "Laity",
+            value: 7.335907335907336,
+            color: "#fad6f9",
+          },
+          {
+            name: "Clergy",
+            value: 8.904109589041095,
+            color: "#fad6f9",
+          },
+        ],
+      },
+      {
+        name: "Demographic shifts (how to balance the needs of different groups)",
+        value: 67,
+        color: "#ecbc88",
+        children: [
+          {
+            name: "Laity",
+            value: 16.216216216216218,
+            color: "#ecbc88",
+          },
+          {
+            name: "Clergy",
+            value: 12.32876712328767,
+            color: "#ecbc88",
+          },
+        ],
+      },
+      {
+        name: "Clergy shortage",
+        value: 20,
+        color: "#bedc4c",
+        children: [
+          {
+            name: "Laity",
+            value: 4.247104247104247,
+            color: "#bedc4c",
+          },
+          {
+            name: "Clergy",
+            value: 5.47945205479452,
+            color: "#bedc4c",
+          },
+        ],
+      },
+      {
+        name: "Continuing education/training for clergy",
+        value: 30,
+        color: "#caa4ab",
+        children: [
+          {
+            name: "Laity",
+            value: 5.405405405405405,
+            color: "#caa4ab",
+          },
+          {
+            name: "Clergy",
+            value: 9.58904109589041,
+            color: "#caa4ab",
+          },
+        ],
+      },
+    ],
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -250,37 +520,7 @@ function App() {
             height: "1000px",
           }}
         >
-          <CirclePacking
-            {...commonProperties}
-            data={testData}
-            enableLabels
-            tooltip={({ id, value, color }) => (
-              <div style={{ backgroundColor: "#666", padding: "8px" }}>
-                <strong style={{ color }}>
-                  {id}: {value}
-                </strong>
-              </div>
-            )}
-            label={(input: any) => {
-              //@ts-ignore
-              return input?.data?.name?.length > 9
-                ? `${input.data?.name?.substring(0, 5)}...`
-                : input?.data?.name;
-            }}
-            labelsSkipRadius={16}
-            // colors={{ scheme: "blue_purple" }}
-            labelsFilter={(label) => label.node.height === 0}
-            // labelComponent={(ar) => <div>{ar.label}</div>}
-            labelTextColor="black"
-            // labelTextColor={{
-            //   from: "color",
-            // }}
-            zoomedId={zoomedId}
-            motionConfig="slow"
-            onClick={(node) => {
-              setZoomedId(zoomedId === node.id ? null : node.id);
-            }}
-          />
+          {CircleChart(staticValues)}
         </div>
         <div
           className="legend"
